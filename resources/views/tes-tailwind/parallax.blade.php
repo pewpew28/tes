79d145afa32d454011f9f384e3234f1d2ab2e329
Revangga <revanggaprakusya527@gmail.com>
2024-04-25 16:40:09 +0700

    tes tailwind

---
+++ resources/views/tes-tailwind/parallax.blade.php
@@ -0,0 +1,12 @@
+<div class="w-full overscroll-contain">
+    <div class="w-full min-h-screen bg-[url(https://th.bing.com/th/id/OIP.to4sk2-s8BtH848mub21yAHaE8?rs=1&pid=ImgDetMain)] bg-cover bg-no-repeat flex justify-center items-center bg-fixed">
+    <h1 class="font-bold text-black text-2xl">Parallax</h1>
+    </div>
+
+    <div class="w-full h-auto bg-black">
+    </div>
+
+    <div class="w-full min-h-screen bg-[url(https://pearlseveryday.files.wordpress.com/2011/06/the-sea.jpg)] bg-cover bg-no-repeat bg-fixed flex justify-center items-center">
+        <h1 class="font-bold text-black text-2xl">Parallax</h1>
+    </div>
+</div>
