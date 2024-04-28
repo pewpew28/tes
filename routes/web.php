2aa9a904458920e4754870f9b74b5912b1a8fdc2
Revangga <revanggaprakusya527@gmail.com>
2024-04-28 14:03:52 +0700

    slicing enak

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [function () {
+    return view('tes-tailwind.index');
+}]);
+
+Route::get('/rotate', [function () {
     return view('rotate');
 }]);
+
 
 Route::get('/tailwind-test', function(){
     return view('tes-tailwind.index');
